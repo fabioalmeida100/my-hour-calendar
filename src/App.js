8641cb84a83e06311fb9581702175d360f53f736
Fabio Almeida <fabioalmeida@fabiodeveloper.com>
2023-06-28 02:29:34 -0300

    Add header

--- src/App.js
+++ src/App.js
@@ -1,48 +1,36 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const today = new Date();
-  const day = String(today.getDate()).padStart(2, '0');
-  const month = String(today.getMonth() + 1).padStart(2, '0'); 
-  const year = today.getFullYear();
-  const hour = today.getHours();
-  const minutes = today.getMinutes();
-  const formatteStartdDate = `${year}-${month}-${day}T${hour}:${minutes}:00`;
-  const formatteEndDate = `${year}-${month}-${day}T${hour}:${minutes}:00`;
-
   const [description, setDescription] = useState('');
-  const [startDate, setStartDate] = useState(formatteStartdDate);
-  const [endDate, setEndDate] = useState(formatteEndDate);
-  const [totalHours, setTotalHours] = useState(0);
-  const [totalMinutes, setTotalMinutes] = useState(0);
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
   const [listEvents, setListEvents] = useState([]);
   const [totalWorked, setTotalWorked] = useState('');
-  const [id, setId] = useState(0);
+  const [id, setId] = useState(1);
 
   useEffect(() => {
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const diff = (end - start)/1000;
-    const hours = Math.floor(diff / 3600) % 24;
-    const minutes = Math.floor(diff / 60) % 60;
-
-    setTotalHours(hours);
-    setTotalMinutes(minutes);
-  }, [startDate, endDate]);
+    fetchEvents();
+  }, []);
 
   useEffect(() => {
     handleTotalWorked();
   }, [listEvents]);
+  
+  const fetchEvents = async () => {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/events`);
+    setListEvents(response.data);
+    handleTotalWorked();
+    setId(Math.max(...response.data.map((event) => event.id)) + 1);
+  }
 
   const handleStartDate = (event) => {
-    console.log(event.target.value);
     setStartDate(event.target.value);
   }
 
-  const handleEndDate = (event) => {
-    console.log(event.target.value);
+  const handleEndDate = (event) => {    
     setEndDate(event.target.value);
   }
 
@@ -50,23 +38,36 @@ function App() {
     setDescription(event.target.value);
   }
 
-  const handleSave = () => {
-    setId(id + 1);
+  const handleSave = async () => {
+    if (!description || !startDate || !endDate) {
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    const start = new Date(startDate);
+    const end = new Date(endDate);
+    const diff = (end - start)/1000;
+    const hours = Math.floor(diff / 3600) % 24;
+    const minutes = Math.floor(diff / 60) % 60;
 
     const event = {
       id: id,
       description,
       startDate,
       endDate,
-      totalHours,
-      totalMinutes,
-      total: `${totalHours}:${totalMinutes}`,
+      total: `${hours}:${minutes}`,
       checked: false
     }
-    setListEvents([...listEvents, event]);
+    
+    await axios.post(`${process.env.REACT_APP_API_URL}/events`, event);
+    await fetchEvents();
+    
+    setStartDate('');
+    setEndDate('');
   }
 
   const handleChecked = (checkbox) => {
+  
     const id = Number(checkbox.target.value);
     const newList = listEvents.map((event) => {
       if(event.id === id) {
@@ -74,53 +75,49 @@ function App() {
       }
       return event;
     });
-    console.log(newList);
+    
     setListEvents(newList);
-
   }
+  
 
-  const handleDeleteAllChecked = () => {
-    const newList = listEvents.filter((event) => {
-      return !event.checked;
+  const handleDeleteAllChecked = async () => {
+    listEvents.forEach((event) => {
+      if(event.checked) {
+        axios.delete(`${process.env.REACT_APP_API_URL}/events/${event.id}`);
+      }
     });
-    setListEvents(newList);
+
+    await fetchEvents();
   }
 
   const handleTotalWorked = () => {
+    if (listEvents && listEvents.length > 0) {
       let totalDateTimeWorked = null;
-
-      listEvents.forEach((event) => {
-        totalDateTimeWorked += (new Date(event.endDate) - new Date(event.startDate));
-      });
+      listEvents.forEach((event) => totalDateTimeWorked += (new Date(event.endDate) - new Date(event.startDate)));
       
       const hours = Math.floor(totalDateTimeWorked / 3600000) % 24;
       const minutes = Math.floor(totalDateTimeWorked / 60000) % 60;
-
+      
       setTotalWorked(`${hours}h:${minutes}m`);
+    }
    }
 
   return (
     <Container className='w-50'>
       <Row>
-        <Col xs={{offset:3}}>
-          <h1>My Hour Calendar</h1>
-        </Col>
-      </Row>
-
-      <Row>
         <Col>
-          <label>Description</label>
+          <label className='mb-2'>Description</label>
           <input type="text" className="form-control" onChange={handleDescription}/>
         </Col>
       </Row>
 
-      <Row>
+      <Row className='mt-2'>
         <Col>
-          <label>Start date</label>
+          <label className='mb-2'>Start date</label>
           <input type="datetime-local" className="form-control" onChange={handleStartDate} value={startDate}/>
         </Col>
         <Col>
-          <label>End date</label>
+          <label className='mb-2'>End date</label>
           <input type="datetime-local" className="form-control" onChange={handleEndDate} value={endDate}/>
         </Col>
       </Row>
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+function Header() {
+    return (
+    <>
+      <Navbar bg="dark" data-bs-theme="dark" className="mb-4">
+        <Container>
+          <Navbar.Brand href="#home"><span className="text-warning">MyHour</span>Calendar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="/tasks">Atividades</Nav.Link>
+            <Nav.Link href="/report">Relatório</Nav.Link>
+            <Nav.Link href="/profile">Perfil</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default Header;
